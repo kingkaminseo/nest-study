3f893e32a5c5def67b41b117acade47240a7f72f
kingkaminseo <rlazmfdl@gmail.com>
2025-11-09 21:53:16 +0900

    ✨ feat: Cats 컨트롤러 라우팅 구현

--- first-nest/src/cats/cats.controller.ts
+++ first-nest/src/cats/cats.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateCatDto } from './CreateCatDto';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This actions returns a #s{id} cat`;
+  }
+
+  @Post()
+  create(@Body() CreateCatDto: CreateCatDto) {
+    return 'This action adds a new cats';
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() CreateCatDto: CreateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
+  }
+}
